19582261e3c5215bb55e1e4bd84693195fac85d9
Oliver Wang <48oliverwang48@gmail.com>
2017-01-25 22:37:05 -0800

    Updated CeilingBot.ino
    Edited program to be self reliant and not dependent on MQTT protocol
    requests.

--- CeilingBot.ino
+++ CeilingBot.ino
@@ -7,75 +7,35 @@
  * column 33, lower: DC motor 1, positive
  * column 33, upper: DC motor 2, positive
  */
-
-/*libraries*/
-#include <ESP8266WiFi.h>
-#include "Adafruit_MQTT.h"
-#include "Adafruit_MQTT_Client.h"
-
-/*Wifi*/
-//#define WLAN_SSID       "guest-SDUHSD"
-//#define WLAN_PASS       ""
-#define WLAN_SSID       "NETGEAR47"
-#define WLAN_PASS       "unusualearth866"
-
-/*MQTT*/
-#define AIO_SERVER      "io.adafruit.com"
-#define AIO_SERVERPORT  1883
-#define AIO_USERNAME    "CeilingBot"
-#define AIO_KEY         "f93aa0c6bcd04926b790004d481e3000"
-
+ 
 /*Pin Definitions*/
 #define PWM_MOTOR_PIN 0 //GPIO0, or D3
 #define LEG1_MOTOR1_PIN 2 //GPIO2, or D4
 #define LEG1_MOTOR2_PIN 5 //GPIO5, or D1
 #define COMMON_LEG2_PIN 13 //GPIO13, or D7
 
-// Create an ESP8266 WiFiClient class to connect to the MQTT server.
-WiFiClient client;
-
-// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
-Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_USERNAME, AIO_KEY);
-
-/*Feed*/
-Adafruit_MQTT_Subscribe testMotor1 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/testMotor1");
-Adafruit_MQTT_Subscribe testMotor2 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/testMotor2");
-Adafruit_MQTT_Subscribe testForward = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/testForward");
-Adafruit_MQTT_Subscribe testBackward = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/testBackward");
-Adafruit_MQTT_Subscribe testYawLeft = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/testYawLeft");
-Adafruit_MQTT_Subscribe testYawRight = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/testYawRight");
+/*Method Definitions*/
+#define TESTFORWARD 1
+#define TESTBACKWARD 2
+#define TESTYAWLEFT 3
+#define TESTYAWRIGHT 4
 /* FEED INFO (assuming using yomafacio's MQTT broker account):
- * testMotor1:
- *   slider that ranges from the values -1000 to 1000
- *   values between -100 and 100 are counted as zero
- *   controls motor 1
- *   direction dictated by value
- * testMotor2:
- *   slider that ranges from the values -1000 to 1000
- *   values between -100 and 100 are counted as zero
- *   controls motor 2
- *   direction dictated by value
  * testForward:
- *   slider that ranges from the values 0 to 10000
- *   values represent duration (milliseconds) of action
  *   controls motor 1 and 2
  *   both motors forward
  * testBackward:
- *   slider that ranges from the values 0 to 10000
- *   values represent duration (milliseconds) of action
  *   controls motor 1 and 2
  *   both motors backward
  * testYawLeft:
- *   slider that ranges from the values 0 to 1000
- *   values represent duration (milliseconds) of action
  *   controls motor 1 and 2
- *   motor 1 backward, motor 2 forward
+ *   motor 1 stop, motor 2 forward
  * testYawRight:
- *   slider that ranges from the values 0 to 1000
- *   values represent duration (milliseconds) of action
  *   controls motor 1 and 2
- *   motor 1 forward, motor 2 backward
+ *   motor 1 forward, motor 2 stop
  */
+
+//METHOD TO RUN (refer above for constant names)
+int method = 2;
 
 void setup() {
   // initialize pins
@@ -85,150 +45,34 @@ void setup() {
   pinMode(COMMON_LEG2_PIN, OUTPUT);
   
   Serial.begin(9600);
-
-  // Connect to WiFi access point.
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(WLAN_SSID);
-
-  WiFi.begin(WLAN_SSID, WLAN_PASS); 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println();
-  Serial.println("WiFi connected");
-  Serial.println("IP address: "); Serial.println(WiFi.localIP());
-  
-  //Setup the MQTT subscription
-  mqtt.subscribe(&testMotor1);
-  mqtt.subscribe(&testMotor2);
-  mqtt.subscribe(&testForward);
-  mqtt.subscribe(&testBackward);
-  mqtt.subscribe(&testYawLeft);
-  mqtt.subscribe(&testYawRight);
+  Serial.println(method);
 }
 
 void loop() { 
-
-  // Ensure the connection to the MQTT server is alive (this will make the first
-  // connection and automatically reconnect when disconnected).  See the MQTT_connect
-  // function definition further below.
-  MQTT_connect();
-
-  // this is our 'wait for incoming subscription packets' busy subloop
-  Adafruit_MQTT_Subscribe *subscription;
-  while ((subscription = mqtt.readSubscription(5000))) {
-    //note: using a switch loop does not work, must use if/else
-    if (subscription == &testMotor1) {
-      Serial.print(F("testMotor1 update speed: "));
-      Serial.println((char *)testMotor1.lastread);
-      int speed_motor1 = atoi((const char *)testMotor1.lastread);
-      if (speed_motor1 < 100 && speed_motor1 > -100) {
-        speed_motor1 = 0;
-        Serial.println("motor1 speed set as zero since -100 < input < 100");
-      }
-      if(speed_motor1 > 0) {
-        digitalWrite(LEG1_MOTOR1_PIN, HIGH);
-        digitalWrite(COMMON_LEG2_PIN, LOW);
-      } else {
-        digitalWrite(LEG1_MOTOR1_PIN, LOW);
-        digitalWrite(COMMON_LEG2_PIN, HIGH);
-        speed_motor1 = -speed_motor1;
-      }
-      analogWrite(PWM_MOTOR_PIN,speed_motor1);
-    } else if (subscription == &testMotor2) {
-      Serial.print(F("testMotor2 update speed: "));
-      Serial.println((char *)testMotor2.lastread);
-      int speed_motor2 = atoi((const char *)testMotor2.lastread);
-      if (speed_motor2 < 100 && speed_motor2 > -100) {
-        speed_motor2 = 0;
-        Serial.println("motor2 speed set as zero since -100 < input < 100");
-      }
-      if(speed_motor2 > 0) {
-        digitalWrite(LEG1_MOTOR2_PIN, HIGH);
-        digitalWrite(COMMON_LEG2_PIN, LOW);
-      } else {
-        digitalWrite(LEG1_MOTOR2_PIN, LOW);
-        digitalWrite(COMMON_LEG2_PIN, HIGH);
-        speed_motor2 = -speed_motor2;
-      }
-      analogWrite(PWM_MOTOR_PIN,speed_motor2);
-    } else if (subscription == &testForward) {
-      Serial.print(F("testForward update duration: "));
-      Serial.println((char *)testForward.lastread);
-      int duration_forward = atoi((const char *)testForward.lastread);
+    if (method == 1) {
+      Serial.println("testForward Initiating...");
       digitalWrite(LEG1_MOTOR1_PIN, HIGH);
       digitalWrite(LEG1_MOTOR2_PIN, HIGH);
       digitalWrite(COMMON_LEG2_PIN, LOW);
       analogWrite(PWM_MOTOR_PIN, 1000);
-      delay(duration_forward);
-      analogWrite(PWM_MOTOR_PIN, 0);
-    } else if (subscription == &testBackward) {
-      Serial.print(F("testBackward update duration: "));
-      Serial.println((char *)testBackward.lastread);
-      int duration_backward = atoi((const char *)testBackward.lastread);
+    } else if (method == 2) {
+      Serial.println("testBackward Initiating...");
       digitalWrite(LEG1_MOTOR1_PIN, LOW);
       digitalWrite(LEG1_MOTOR2_PIN, LOW);
       digitalWrite(COMMON_LEG2_PIN, HIGH);
       analogWrite(PWM_MOTOR_PIN, 1000);
-      delay(duration_backward);
-      analogWrite(PWM_MOTOR_PIN, 0);
-    } else if (subscription == &testYawLeft) {
-      Serial.print(F("testYawLeft update duration: "));
-      Serial.println((char *)testYawLeft.lastread);
-      int duration_left = atoi((const char *)testYawLeft.lastread);
+    } else if (method == 3) {
+      Serial.println("testYawLeft Initiating...");
       digitalWrite(LEG1_MOTOR1_PIN, LOW);
       digitalWrite(LEG1_MOTOR2_PIN, HIGH);
       digitalWrite(COMMON_LEG2_PIN, LOW);
       analogWrite(PWM_MOTOR_PIN, 1000);
-      delay(duration_left);
-      analogWrite(PWM_MOTOR_PIN, 0);
-    } else if (subscription == &testYawRight) {
-      Serial.print(F("testYawRight update duration: "));
-      Serial.println((char *)testYawRight.lastread);
-      int duration_right = atoi((const char *)testYawRight.lastread);
+    } else if (method == 4) {
+      Serial.println("testYawRight Initating...");
       digitalWrite(LEG1_MOTOR1_PIN, HIGH);
       digitalWrite(LEG1_MOTOR2_PIN, LOW);
       digitalWrite(COMMON_LEG2_PIN, LOW);
       analogWrite(PWM_MOTOR_PIN, 1000);
-      delay(duration_right);
-      analogWrite(PWM_MOTOR_PIN, 0);
     }
-  }
-  
-  // ping the server to keep the mqtt connection alive
-  if(! mqtt.ping()) {
-    mqtt.disconnect();
-  }
-
-} //end loop()
-
-
-// Function to connect and reconnect as necessary to the MQTT server.
-// Should be called in the loop function and it will take care if connecting.
-void MQTT_connect() {
-  int8_t ret;
-
-  // Stop if already connected.
-  if (mqtt.connected()) {
-    return;
-  }
-
-  Serial.print("Connecting to MQTT... ");
-
-  uint8_t retries = 3;
-  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
-       Serial.println(mqtt.connectErrorString(ret));
-       Serial.println("Retrying MQTT connection in 5 seconds...");
-       mqtt.disconnect();
-       delay(5000);  // wait 5 seconds
-       retries--;
-       if (retries == 0) {
-         // basically die and wait for WDT to reset me
-         while (1);
-       }
-  }
-  Serial.println("MQTT Connected!");
+    delay(100000);
 }
