756ca31910f248d19dd8f0226ac5e21fea25d6da
Oliver Wang <48oliverwang48@gmail.com>
2017-02-05 12:22:52 -0800

    Added buffer and change to autorun structure
    0.5 second buffer between running each motor to prevent shorts from
    occuring. removed manual method select, automatically cycles every 5(+
    .5) seconds

--- CeilingBot.ino
+++ CeilingBot.ino
@@ -14,29 +14,6 @@
 #define LEG1_MOTOR2_PIN 5 //GPIO5, or D1
 #define COMMON_LEG2_PIN 13 //GPIO13, or D7
 
-/*Method Definitions*/
-#define TESTFORWARD 1
-#define TESTBACKWARD 2
-#define TESTYAWLEFT 3
-#define TESTYAWRIGHT 4
-/* FEED INFO (assuming using yomafacio's MQTT broker account):
- * testForward:
- *   controls motor 1 and 2
- *   both motors forward
- * testBackward:
- *   controls motor 1 and 2
- *   both motors backward
- * testYawLeft:
- *   controls motor 1 and 2
- *   motor 1 stop, motor 2 forward
- * testYawRight:
- *   controls motor 1 and 2
- *   motor 1 forward, motor 2 stop
- */
-
-//METHOD TO RUN (refer above for constant names)
-int method = 1;
-
 void setup() {
   // initialize pins
   pinMode(PWM_MOTOR_PIN, OUTPUT);
@@ -45,34 +22,43 @@ void setup() {
   pinMode(COMMON_LEG2_PIN, OUTPUT);
   
   Serial.begin(9600);
-  Serial.println(method);
 }
 
 void loop() { 
-    if (method == 1) {
-      Serial.println("testForward Initiating...");
-      digitalWrite(LEG1_MOTOR1_PIN, HIGH);
-      digitalWrite(LEG1_MOTOR2_PIN, HIGH);
-      digitalWrite(COMMON_LEG2_PIN, LOW);
-      analogWrite(PWM_MOTOR_PIN, 1000);
-    } else if (method == 2) {
-      Serial.println("testBackward Initiating...");
-      digitalWrite(LEG1_MOTOR1_PIN, LOW);
-      digitalWrite(LEG1_MOTOR2_PIN, LOW);
-      digitalWrite(COMMON_LEG2_PIN, HIGH);
-      analogWrite(PWM_MOTOR_PIN, 1000);
-    } else if (method == 3) {
-      Serial.println("testYawLeft Initiating...");
-      digitalWrite(LEG1_MOTOR1_PIN, LOW);
-      digitalWrite(LEG1_MOTOR2_PIN, HIGH);
-      digitalWrite(COMMON_LEG2_PIN, LOW);
-      analogWrite(PWM_MOTOR_PIN, 1000);
-    } else if (method == 4) {
-      Serial.println("testYawRight Initating...");
-      digitalWrite(LEG1_MOTOR1_PIN, HIGH);
-      digitalWrite(LEG1_MOTOR2_PIN, LOW);
-      digitalWrite(COMMON_LEG2_PIN, LOW);
-      analogWrite(PWM_MOTOR_PIN, 1000);
-    }
-    delay(100000);
+  Serial.println("testForward Initiating...");
+  digitalWrite(LEG1_MOTOR1_PIN, HIGH);
+  digitalWrite(LEG1_MOTOR2_PIN, HIGH);
+  digitalWrite(COMMON_LEG2_PIN, LOW);
+  analogWrite(PWM_MOTOR_PIN, 1000);
+  delay(5000);
+  bufferNull();
+  Serial.println("testBackward Initiating...");
+  digitalWrite(LEG1_MOTOR1_PIN, LOW);
+  digitalWrite(LEG1_MOTOR2_PIN, LOW);
+  digitalWrite(COMMON_LEG2_PIN, HIGH);
+  analogWrite(PWM_MOTOR_PIN, 1000);
+  delay(5000);
+  bufferNull();
+  Serial.println("testYawLeft Initiating...");
+  digitalWrite(LEG1_MOTOR1_PIN, LOW);
+  digitalWrite(LEG1_MOTOR2_PIN, HIGH);
+  digitalWrite(COMMON_LEG2_PIN, LOW);
+  analogWrite(PWM_MOTOR_PIN, 1000);
+  delay(5000);
+  bufferNull();
+  Serial.println("testYawRight Initating...");
+  digitalWrite(LEG1_MOTOR1_PIN, HIGH);
+  digitalWrite(LEG1_MOTOR2_PIN, LOW);
+  digitalWrite(COMMON_LEG2_PIN, LOW);
+  analogWrite(PWM_MOTOR_PIN, 1000);
+  bufferNull();
 }
+
+void bufferNull() {
+  digitalWrite(LEG1_MOTOR1_PIN, LOW);
+  digitalWrite(LEG1_MOTOR2_PIN, LOW);
+  digitalWrite(COMMON_LEG2_PIN, LOW);
+  analogWrite(PWM_MOTOR_PIN, 0);
+  delay(500);
+}
+
